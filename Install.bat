f35aec68f6f23002556650fa634ba42e622de099
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-27 13:08:48 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,16 @@
+color a
+cd C:\Users\%USERNAME%\Downloads
+curl https://raw.githubusercontent.com/Twig6943/Non-Bloated-Bluestacks-3/main/Google.reg -o Google.reg
+curl https://raw.githubusercontent.com/Twig6943/Non-Bloated-Bluestacks-3/main/Pre.reg -o Pre.reg
+curl https://cdn3.bluestacks.com/downloads/3.7.44.1625/BlueStacks-Installer_BS3_native.exe -o bs3.exe
+C:\Users\%USERNAME%\Downloads\bs3.exe
+C:\Users\%USERNAME%\Downloads\Google.reg
+C:\Users\%USERNAME%\Downloads\Pre.reg
+echo restart the program if it still requires you to sign in with a google account
+echo restart the program if it still requires you to sign in with a google account
+echo restart the program if it still requires you to sign in with a google account
+echo restart the program if it still requires you to sign in with a google account
+echo restart the program if it still requires you to sign in with a google account
+echo restart the program if it still requires you to sign in with a google account
+pause
+
